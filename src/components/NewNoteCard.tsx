941006207ea68ea1825bc01a3d2bcf4b5828a5bc
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-02-07 08:21:30 -0300

    aula 2 finalizada

---
+++ src/components/NewNoteCard.tsx
@@ -0,0 +1,89 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "sonner";
+
+export function NewNoteCard() {
+    const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+    const [content, setContent] = useState("");
+
+    function handleStartEditor() {
+        setShouldShowOnboarding(false);
+    }
+
+    function handleContentChanged(e: ChangeEvent<HTMLTextAreaElement>) {
+        setContent(e.target.value);
+
+        if (e.target.value === "") {
+            setShouldShowOnboarding(!false);
+        }
+    }
+
+    function handleSaveNote(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(content);
+        toast.success("Nota salva com sucesso!");
+    }
+
+    return (
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-lg bg-slate-700 gap-3 p-5 overflow-hidden relative text-left flex flex-col hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-200">
+                    Adicionar nota
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                    Grave uma nota em áudio que será convertida para texto
+                    automaticamente.
+                </p>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t pointer-events-none from-black/40 to-black/0" />
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+                <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-lg flex flex-col outline-none">
+                    <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                        <X className="size-5" />
+                    </Dialog.Close>
+                    <form
+                        onSubmit={handleSaveNote}
+                        className="flex flex-col flex-1"
+                    >
+                        <div className="flex flex-1 flex-col gap-3 p-5">
+                            <span className="text-sm font-medium text-slate-200">
+                                Adicionar nota
+                            </span>
+                            {shouldShowOnboarding ? (
+                                <p className="text-sm leading-6 text-slate-400 font-medium">
+                                    Comece{" "}
+                                    <button className="text-lime-400 font-bold hover:underline">
+                                        gravando uma nota
+                                    </button>{" "}
+                                    em áudio ou se preferir{" "}
+                                    <button
+                                        className="text-lime-400 font-bold hover:underline"
+                                        onClick={handleStartEditor}
+                                    >
+                                        utilize apenas texto
+                                    </button>
+                                    .
+                                </p>
+                            ) : (
+                                <textarea
+                                    autoFocus
+                                    className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                                    onChange={handleContentChanged}
+                                />
+                            )}
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-bold hover:bg-lime-500"
+                        >
+                            Salvar nota
+                        </button>
+                    </form>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    );
+}
