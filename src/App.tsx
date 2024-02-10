6c8fa5104c9e285ec046fd1a22af6b78f08a1c92
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-02-10 11:01:35 -0300

    algumas pequenas atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/NewNoteCard";
 import { NoteCard } from "./components/NoteCard";
 
 export function App() {
+    const [notes, setNotes] = useState([
+        {
+            id: 1,
+            date: new Date(),
+            content: "Hello World",
+        },
+        {
+            id: 2,
+            date: new Date(),
+            content: "Terminar trilha de React e começar a de Node",
+        },
+    ]);
+
+    function onNoteCreated(content: string) {
+
+        const newNote = {
+            id: Math.random(),
+            date: new Date(),
+            content,
+        };
+
+        setNotes([newNote, ...notes]);
+    }
+
     return (
         <div className="mx-auto max-w-6xl my-12 space-y-6">
             <img src={logo} alt="NLW Expert" />
@@ -18,13 +43,10 @@ export function App() {
             <div className="h-px bg-slate-700" />
 
             <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-                <NewNoteCard />
-                <NoteCard
-                    note={{
-                        date: new Date(),
-                        content: "Hello World",
-                    }}
-                />
+                <NewNoteCard onNoteCreated={onNoteCreated}/>
+                {notes.map((item) => (
+                    <NoteCard key={item.id} note={item} />
+                ))}
             </div>
         </div>
     );
